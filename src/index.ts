ab4f1904683a108f6ddc914ac51128942e496b92
Shadman Shadab <oneshadab@gmail.com>
2021-02-12 17:27:39 +0600

    Add commander for command line options

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import path from 'path';
+import { Command } from 'commander';
+
 import SiteGenerator from './SiteGenerator';
 
 async function main() {
-  const rootDir = 'example';
+  const program = new Command();
+  program
+    .version('0.0.1')
+    .option('-w, --watch', 'watch for changes')
+    .option('-d, --dir', 'site directory');
+  program.parse(process.argv);
 
-  const generator = new SiteGenerator({
-    layoutDir: path.join(rootDir, 'layouts'),
-    pagesDir: path.join(rootDir, 'pages'),
-    outputDir: path.join(rootDir, 'out'),
-    staticDir: path.join(rootDir, 'static'),
-  });
+  const argOptions = program.opts();
+
+  const siteDir: string = argOptions.dir ?? 'example';
+  const siteConfig = {
+    layoutDir: path.join(siteDir, 'layouts'),
+    pagesDir: path.join(siteDir, 'pages'),
+    outputDir: path.join(siteDir, 'out'),
+    staticDir: path.join(siteDir, 'static'),
+  };
+
+  const generator = new SiteGenerator(siteConfig);
 
   await generator.generate();
 }
